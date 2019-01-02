01c2d1fa2790dc5139d1aec6bd9630de89ec86ce
davy1ex <dezy1ex@gmail.com>
2019-01-02 19:29:29 +0400

    initial commit

---
+++ bot.py
@@ -0,0 +1,21 @@
+import vk_api 
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+
+def main():
+    login = input('Login: ')
+    password = input('Password: ')
+    session = vk_api.VkApi(login, password)
+    session.auth()
+    vk = session.auth()
+    lp = VkLongPoll(session)
+    for event in lp.listen():
+        if event.type == VkEventType.MESSAGE_NEW:
+            print('Новое сообщение: ', event.text)
+
+    # if vk.users.get(user_id='154597302', fields='online'):
+    #     vk.messages.send(user_id='154597302', message='хуй', random_id='235')
+
+
+if __name__ == '__main__':
+    main()
